d64815c8416b74303affb63a4228bc2e4be5ca55
David Farrell <dfarrell@davidlearnsgames.com>
2013-02-26 22:08:37 +0000

    online setup

--- web/utility/database_config.php
+++ web/utility/database_config.php
@@ -5,11 +5,11 @@ $GLOBALS['MYSQL_DATABASE'] = "career_quest";
 $GLOBALS['MYSQL_USER'] = "root";
 $GLOBALS['MYSQL_PASSWORD'] = "root";
 
-if (false) {
+if (true) { 
 
 $GLOBALS['MYSQL_HOST'] = "mysql.davidlearnsgames.com";
 $GLOBALS['MYSQL_PORT'] = "8889";
-$GLOBALS['MYSQL_DATABASE'] = "bodforum";
+$GLOBALS['MYSQL_DATABASE'] = "cqgame";
 $GLOBALS['MYSQL_USER'] = "dfarrell";
 $GLOBALS['MYSQL_PASSWORD'] = "bOUncey179";
 }
