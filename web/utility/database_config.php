e2291fd5f967bd63c6a80c15e292ea9fd5574542
David Farrell <dfarrell@davidlearnsgames.com>
2013-04-23 16:24:30 +0100

    done

--- web/utility/database_config.php
+++ web/utility/database_config.php
@@ -15,12 +15,21 @@ $GLOBALS['MYSQL_PASSWORD'] = "bOUncey179";
 }
 
 // testing live local
-if (true) { 
+if (false) { 
 
 $GLOBALS['MYSQL_HOST'] = "localhost";
 $GLOBALS['MYSQL_PORT'] = "8889";
 $GLOBALS['MYSQL_DATABASE'] = "cqlive";
 $GLOBALS['MYSQL_USER'] = "root";
 $GLOBALS['MYSQL_PASSWORD'] = "root";
+}
+// alt online
+if (false) { 
+
+$GLOBALS['MYSQL_HOST'] = "mysql.davidlearnsgames.com";
+$GLOBALS['MYSQL_PORT'] = "8889";
+$GLOBALS['MYSQL_DATABASE'] = "cqbackup";
+$GLOBALS['MYSQL_USER'] = "dfarrell";
+$GLOBALS['MYSQL_PASSWORD'] = "bOUncey179";
 }
 ?>
